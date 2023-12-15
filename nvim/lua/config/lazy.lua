f6b4465138a6157fb6557b88351b481179159d21
Silvano Junior <silvanomsjr@gmail.com>
2023-12-14 22:22:15 -0300

    alterei tema alacritty e adicionei lsp/mason no nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -39,10 +39,10 @@ local plugins = {
   'windwp/nvim-ts-autotag',
   'windwp/nvim-autopairs',
   {
-      'numToStr/Comment.nvim',
-      opts = {
-      },
-      lazy = false,
+    'numToStr/Comment.nvim',
+    opts = {
+    },
+    lazy = false,
   },
   {
     "folke/which-key.nvim",
@@ -63,6 +63,30 @@ local plugins = {
     dependencies = { 
       'nvim-lua/plenary.nvim'
     }
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+    }
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'saadparwaiz1/cmp_luasnip',
+    }
+  },
+  {
+    'williamboman/mason.nvim'
+  },
+  {
+    'williamboman/mason-lspconfig.nvim'
   },
 }
 
--- nvim/plugin/whichkey.lua
+++ nvim/plugin/whichkey.lua
@@ -3,14 +3,20 @@ local wk = require 'which-key'
 
 local mappings = {
   e = { '<cmd>NvimTreeToggle<cr>', 'File Explorer' },
-  q = { '<cmd>quit<cr>', 'Sair' },
+  q = { '<cmd>quitall<cr>', 'Sair de tudo' },
   t = {
+    name = 'Telescope',
     f = { '<cmd>Telescope find_files<cr>', 'Encontrar arquivos'},
     l = { '<cmd>Telescope live_grep<cr>', 'Live Grep' },
     k = { '<cmd>Telescope keymaps<cr>', 'Keymaps' },
     h = { '<cmd>Telescope help_tags<cr>', 'Help tags' },
     c = { '<cmd>Telescope commands<cr>', 'Commands' },
-  }
+  },
+  m = {
+    name = 'Mason',
+    p = { '<cmd>Mason<cr>', 'Abrir Mason' },
+    u = { '<cmd>MasonUpdate<cr>', 'Update Mason' },
+  },
 }
 
 wk.register(mappings, { prefix = '<leader>'} )
