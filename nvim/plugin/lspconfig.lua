205322fe3fe371a2190a9ae715cd081ce096b54a
Silvano Junior <silvanomsjr@gmail.com>
2023-12-15 15:29:33 -0300

    fix dnv

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -54,7 +54,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
@@ -80,7 +79,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
-local state =''
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
